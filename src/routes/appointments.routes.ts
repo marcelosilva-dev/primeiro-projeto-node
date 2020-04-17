7d56be61afee4fcf77ad90c894347cae7c07a9dc
Marcelo Oliveira <marceloeduardo244@gmail.com>
2020-04-16 21:22:47 -0300

    concluido

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,34 @@
+import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentRepository from '../repositories/AppointmentsRepository';
+
+const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSamedate = appointmentsRepository.findByDate(
+    parsedDate,
+  );
+
+  if (findAppointmentInSamedate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment has already booked ' });
+  }
+
+  const appointment = appointmentsRepository.create(provider, parsedDate);
+
+  return response.json({ appointment });
+});
+
+export default appointmentsRouter;
